733377d651b65888e9357856d1ae7e84bdc20bee
Martin Maxime <martinmaxime62@gmail.com>
2023-11-30 16:11:07 +0100

    Ajout ForceDeVente

---
+++ VitaTek/sous-classes/ClasseForceDeVente.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClasseVisiteur
+{
+    public class Secteur
+    {
+        //attributs
+        private String nom, laSituationFamilliale;
+        private int numero, nbEnfantACharge;
+        private DateTime dateEmbauche, dateNaissance;
+        //methodes
+
+        //constructeurs
+
+        //methodes accès varibles privées
+    }
+}
