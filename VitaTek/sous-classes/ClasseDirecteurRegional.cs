5bfadd24314c315fed5551d2b0a1398edb70a2dd
KelhianD <kelhian.dubreucq@malrauxbethune.fr>
2023-11-30 16:28:11 +0100

    merge

--- VitaTek/sous-classes/ClasseDirecteurRegional.cs
+++ VitaTek/sous-classes/ClasseDirecteurRegional.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using ClasseForceDeVente;
+
 namespace ClasseDirecteurRegional
 {
-	public class DirecteurRegional : ForceDeVente
+	public class DirecteurRegional 
 
 	{
 		//attributs
--- VitaTek/sous-classes/ClasseSecteur.cs
+++ VitaTek/sous-classes/ClasseSecteur.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace ClasseVisiteur
+namespace ClasseSecteur
 {
     public class Secteur
     {
